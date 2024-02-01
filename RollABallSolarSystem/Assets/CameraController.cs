d63d4998d2c745320bd197a578bb2b6939e77f84
ProTime00 <108126773+ProTime00@users.noreply.github.com>
2024-01-31 18:51:40 -0800

    fgini

--- RollABallSolarSystem/Assets/CameraController.cs
+++ RollABallSolarSystem/Assets/CameraController.cs
@@ -1,5 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Reference to the player GameObject.
+    public GameObject player;
+
+    // The distance between the camera and the player.
+    private Vector3 offset;
+
+    // Start is called before the first frame update.
+    void Start()
     {
         // Calculate the initial offset between the camera's position and the player's position.
         offset = transform.position - player.transform.position; 
